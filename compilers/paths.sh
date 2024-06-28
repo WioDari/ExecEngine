23aaf0f557556b93d7837616946cb96ef82f7af7
WioDari <wiosnagd97@gmail.com>
2024-06-28 04:51:17 -0400

    Update 0.1.3. Check CHANGELOG for more info

---
+++ compilers/paths.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export PATH=/usr/local/gcc-13.3.0/bin: \
+            /usr/local/gcc-13.3.0/bin: \
+            /usr/local/python-2.7.17/bin: \
+            /usr/local/python-3.12.4/bin: \
+            /usr/local/nasm-2.16.03/bin: \
+            /usr/local/php-8.3.8/bin: \
+            /usr/local/php-8.3.8/bin: \
+            /usr/local/bash-5.2.21/bin: \
+            /usr/local/openjdk22/bin: \
+            /usr/local/pypy3.10/bin: \
+            /usr/local/pypy2.7/bin: \
+            /usr/local/node-20.15.0/bin: \
+            /usr/local/ruby-3.3.3/bin: \
+            /usr/local/dart-3.4.4/dart-sdk/bin: \
+            /usr/local/go-1.22.4/bin: \
+            /usr/local/rust-1.79.0/bin: \
+            /usr/local/kotlin-2.0.0/kotlinc/bin: \
+            /usr/local/fpc-3.2.2/bin:$PATH
